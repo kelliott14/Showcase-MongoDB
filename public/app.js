62c039df93c8eb9418254867609c4d690e37afa2
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-11-28 16:20:07 +1100

    finishing for today. Need to work on modal next

--- public/app.js
+++ public/app.js
@@ -8,5 +8,17 @@ $(document).ready(function() {
         }).then(function(data) {
             location.reload();
         })
-    })
+    });
+
+    $(".collapseIcon").on("click", function(){
+        if ($(".favBox").attr("visible-status") == "show"){
+            $(".favBox").hide("slow", function(){});
+            $(".favBox").attr("visible-status", "hide");
+            $(".collapseIcon").css({"transform": "rotate(-180deg)"});
+        }else {
+            $(".favBox").show("slow", function(){});
+            $(".favBox").attr("visible-status", "show");
+            $(".collapseIcon").css({"transform": "rotate(0deg)"});
+        }
+    });
 });
